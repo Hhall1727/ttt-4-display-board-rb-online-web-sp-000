8d7a7a4feeae68b146287a35066beeba2db8878a
Halie Hall <halierae17@outlook.com>
2021-03-23 15:57:28 +0000

    Automatically backed up by Learn

--- lib/display_board.rb
+++ lib/display_board.rb
@@ -2,9 +2,4 @@
 # out the current state.
 board = [" ", " ", "x", " ", "x", " ", "x", " ", " "]
 def display_board(board)
-  puts "#{board[0]} #{board[1]} #{board[2]}"
-  prints -----------
-  puts "#{board[3]} #{board[4]} #{board[5]}"
-  prints -----------
-  puts "#{board[6]} #{board[7]} #{board[8]}"
 end
