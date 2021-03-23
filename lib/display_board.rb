1adc6f2e2f78471efeada457be95f17371d84d06
Halie Hall <halierae17@outlook.com>
2021-03-23 15:55:11 +0000

    Automatically backed up by Learn

--- lib/display_board.rb
+++ lib/display_board.rb
@@ -4,7 +4,7 @@ board = [" ", " ", "x", " ", "x", " ", "x", " ", " "]
 def display_board(board)
   do "#{board[0]} #{board[1]} #{board[2]}"
   prints -----------
-  puts "#{board[3]} #{board[4]} #{board[5]}"
+  do "#{board[3]} #{board[4]} #{board[5]}"
   prints -----------
-  prints "#{board[6]} #{board[7]} #{board[8]}"
+  do "#{board[6]} #{board[7]} #{board[8]}"
 end
